555b53e3bdb05244c4456baf54922bf72e2cafc3
nvirdi2 <71736420+nvirdi2@users.noreply.github.com>
2021-01-28 18:58:04 -0500

    Update Employee.h

--- Employee.h
+++ Employee.h
@@ -20,7 +20,6 @@ namespace sdds {
         double m_salary;
     };
 
-
    //sorts the dynamic array of employees based on the GPA of the employees.
    void sort();
    // loads a employee structue with its values from the file
@@ -28,7 +27,6 @@ namespace sdds {
    // allocates the dyanmic array of employees and loads all the file
    // recoreds into the array
    bool load();
-
    // TODO: Declare the prototype for the display function that
    // displays a employee record on the screen:
    void display(const Employee& employee);
